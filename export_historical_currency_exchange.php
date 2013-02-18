eb51ff44a6392fa5e99dd00d9ae556d5f40d2bd5
Marc Jarpa <marc.jarpa@socialpoint.es>
2013-02-18 16:30:23 +0100

    Minor changes

--- export_historical_currency_exchange.php
+++ export_historical_currency_exchange.php
@@ -4,15 +4,18 @@
 // The files are updated daily between 2.15 p.m. and 3.00 p.m. CET
 
 // Last 90 days
-$xml=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml");
-// Since 1999
-//$xml=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml");
+$xml = simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml");
+// Uncomment next line to export from 1999
+//$xml = simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml");
+
+// End line
+$endLine = "\n";
 
 foreach ($xml->Cube->Cube as $day) {
-    echo "-------------- For day " . $day["time"] . "--------------\n";
+    echo "-------------- For day " . $day["time"] . "--------------$endLine";
     foreach($day->Cube as $rate){
-        //echo '1&euro;='.$rate["rate"].' '.$rate["currency"].'<br/>';
-        echo "1€ = " . $rate["rate"] . "" . $rate["currency"] . "\n";
+        echo "1€ = " . $rate["rate"] . "" . $rate["currency"] . "$endLine";
+        // Store in database fields $day["time"], $rate["rate"] and $rate["currency"]
     }
 }
 ?>
